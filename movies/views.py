4ba977e70cc7cd9802fc77b2078cc8cf23f38d87
Adnan Alam Ansari <adnanalam326@gmail.com>
2020-12-23 11:41:48 -0300

    adding template

--- movies/views.py
+++ movies/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Movie
 
-# Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    movies = Movie.objects.all()
+    output = ', '.join([movie.title for movie in movies])
+    return render(request, 'movies/index.html', {'movies': movies})
