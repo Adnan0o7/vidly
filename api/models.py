ebdca398bdad98910c7c4208742e0a3650a2d07d
Adnan Alam Ansari <adnanalam326@gmail.com>
2020-12-23 23:00:13 -0300

    adding movies model

--- api/models.py
+++ api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from tastypie.recources import ModelResource
+from tastypie.resources import ModelResource
+from movies.models import Movie
 
 class MovieResource(ModelResource):
   class Meta:
