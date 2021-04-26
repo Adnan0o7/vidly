4b640802b4e2cd90ca715a91760d881c153012bb
Adnan Alam Ansari <adnanalam326@gmail.com>
2021-04-26 15:46:46 -0300

    fixed syntactical error using autopep8
    Following python pep8 conventions

--- movies/urls.py
+++ movies/urls.py
@@ -6,4 +6,4 @@ app_name = 'movies'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:movie_id>', views.detail, name='detail')
-]   
+]
--- movies/views.py
+++ movies/views.py
@@ -1,11 +1,11 @@
-from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404
 from .models import Movie
 
+
 def index(request):
     movies = Movie.objects.all()
-    output = ', '.join([movie.title for movie in movies])
     return render(request, 'movies/index.html', {'movies': movies})
+
 
 def detail(request, movie_id):
     movie = get_object_or_404(Movie, pk=movie_id)
